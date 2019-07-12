50f65bb56ae8423f3f08e6560f3b1d8cc4629941
Adam Yeats <adam@appsignal.com>
2019-07-12 23:24:23 +0200

    WIP: Add Preact integration

---
+++ packages/preact/src/ErrorBoundary.tsx
@@ -0,0 +1,39 @@
+import { Component } from "preact"
+import { Props, State } from "./types/component"
+
+const DEFAULT_ACTION = "ErrorBoundary"
+
+export class ErrorBoundary extends Component<Props, State> {
+  state = { error: undefined }
+
+  static defaultProps = {
+    action: DEFAULT_ACTION
+  }
+
+  public componentDidCatch(error: Error, info: any): void {
+    const { instance: appsignal, action } = this.props
+    const { name, message, stack } = error
+    const span = appsignal.createSpan()
+
+    span
+      .setAction(action !== "" ? action : DEFAULT_ACTION)
+      .setError({
+        name,
+        message,
+        stack
+      })
+      .setTags({ framework: "Preact" })
+
+    appsignal.send(span)
+
+    this.setState({ error })
+  }
+
+  public render(): React.ReactNode {
+    if (this.state.error) {
+      return this.props.fallback ? this.props.fallback(this.state.error) : null
+    }
+
+    return this.props.children
+  }
+}
