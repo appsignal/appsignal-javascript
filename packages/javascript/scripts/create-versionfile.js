cb96f94ff6ea9cb2cdfecf98c3c655d245e22281
Adam Yeats <xadamy@users.noreply.github.com>
2019-07-17 10:30:16 +0200

    Update packages/javascript/scripts/create-versionfile.js
    Co-Authored-By: Tom de Bruijn <tom@tomdebruijn.com>

--- packages/javascript/scripts/create-versionfile.js
+++ packages/javascript/scripts/create-versionfile.js
@@ -7,6 +7,6 @@ const data = `export const VERSION = "${pkg.version}"`
 
 fs.writeFile(`${process.cwd()}/src/version.ts`, data, "utf8", err => {
   if (err) throw err
-  console.log("The file has been saved!")
+  console.log("The VERSION file has been saved!")
   process.exit(0)
 })
