d82d891f3b7c0a5a53b05e01c7bab4bacf686129
Adam Yeats <adam@appsignal.com>
2019-07-24 13:47:35 +0200

    Remove types for Stimulus for now

--- packages/stimulus/src/index.ts
+++ packages/stimulus/src/index.ts
@@ -1,16 +1,9 @@
-import Appsignal from "@appsignal/javascript"
-import { Span } from "@appsignal/javascript/src/span"
-import { Application } from "stimulus"
-
-export function installErrorHandler(
-  appsignal: Appsignal,
-  application: Application
-) {
+export function installErrorHandler(appsignal: any, application: any) {
   const DEFAULT_ACTION = "Application.handleError"
   const prevHandler = application.handleError
 
   application.handleError = function(error: Error, message: string) {
-    const span = appsignal.createSpan((span: Span) =>
+    const span = appsignal.createSpan((span: any) =>
       span
         .setAction(DEFAULT_ACTION)
         .setTags({ framework: "Stimulus", message })
