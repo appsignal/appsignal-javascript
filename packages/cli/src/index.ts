d301b757e02501da28a824cb387a7727745be58e
Adam Yeats <adam@appsignal.com>
2020-07-30 11:50:12 +0200

    Add @appsignal/cli package and diagnose command wrapper
    WIP: CLI

---
+++ packages/cli/src/index.ts
@@ -0,0 +1,30 @@
+import commander, { Command } from "commander"
+
+import { install } from "./commands/install"
+import { diagnose } from "./commands/diagnose"
+
+export class CLI {
+  private cmd: commander.Command
+
+  constructor({ name, version }: { name: string; version: string }) {
+    this.cmd = new Command(name).version(version)
+  }
+
+  public run() {
+    this.cmd
+      .command("install [variant]")
+      .description("installs AppSignal")
+      .action(install)
+
+    this.cmd
+      .command("diagnose")
+      .description("runs the diagnose command (Node.js integration only)")
+      .option("-e, --environment <env>", "Which environment to use")
+      .option("-k, --api-key <key>", "Which API key to use")
+      .action(diagnose)
+
+    this.cmd.parse(process.argv)
+
+    return this
+  }
+}
