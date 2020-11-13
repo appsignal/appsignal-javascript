b5a57f867e075656e64484ffd69dd29336467633
Adam Yeats <adam@appsignal.com>
2020-11-13 01:12:34 +0100

    Remove demo logic and call appsignal.demo() instead

--- packages/cli/src/installers/node/demo.ts
+++ packages/cli/src/installers/node/demo.ts
@@ -8,44 +8,9 @@ const {
 } = require(`${process.cwd()}/node_modules/@appsignal/nodejs/dist`)
 
 const appsignal = new Appsignal({ active: true })
-const tracer = appsignal.tracer()
-
-function createPerformanceSample() {
-  // @TODO: fix up `any` type
-  tracer.withSpan(tracer.createSpan(), (span: any) => {
-    span
-      .setName("GET /demo")
-      .setCategory("process_request.http")
-      .set("demo_sample", true)
-      .setSampleData("environment", { method: "GET", request_path: "/demo" })
-
-    tracer.withSpan(span.child(), (child: any) => {
-      child.setCategory("query.mongodb")
-      setTimeout(() => child.close(), 50)
-    })
-
-    setTimeout(() => span.close(), 100)
-  })
-}
-
-function createErrorSample() {
-  // @TODO: fix up `any` type
-  tracer.withSpan(tracer.createSpan(), (span: any) => {
-    span
-      .setName("GET /demo")
-      .setCategory("process_request.http")
-      .addError(
-        new Error(
-          "Hello world! This is an error used for demonstration purposes."
-        )
-      )
-      .close()
-  })
-}
 
 try {
-  createPerformanceSample()
-  createErrorSample()
+  appsignal.demo()
 
   // cull the process automatically after 90 secs
   setTimeout(() => process.exit(0), 90 * 1000)
