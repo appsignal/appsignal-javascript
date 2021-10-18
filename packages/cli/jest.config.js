1575def20dd7ca6c97d56cdf87cf66981891020b
Luismi Ramírez <luismi.ramirez@protonmail.com>
2021-10-18 09:31:05 +0200

    Apply prettier suggested changes
    Ran Prettier the same way it runs on CI and applied all suggested
    changes.

--- packages/cli/jest.config.js
+++ packages/cli/jest.config.js
@@ -1,9 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
-  roots: [
-    "<rootDir>/src"
-  ],
+  roots: ["<rootDir>/src"],
   transform: {
     "^.+\\.tsx?$": "ts-jest"
   }
