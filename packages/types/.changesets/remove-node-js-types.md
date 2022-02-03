d0660e186b95521a1db8f037effec565fb945bbc
Tom de Bruijn <tom@tomdebruijn.com>
2022-02-03 09:44:12 +0100

    Remove Node.js types from types package
    We moved all types to the Node.js package in PRs:
    
    - https://github.com/appsignal/appsignal-nodejs/pull/451
    - https://github.com/appsignal/appsignal-nodejs/pull/587
    
    Let's remove them from the types package to avoid confusion. People can
    instead use the types/interfaces the Node.js package exports if need.

---
+++ packages/types/.changesets/remove-node-js-types.md
@@ -0,0 +1,6 @@
+---
+bump: "major"
+type: "remove"
+---
+
+Remove the Node.js types from the `@appsignal/types` package. These types are now included in the `@appsignal/nodejs` package instead.
--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,17 +1,7 @@
 export { HashMap, HashMapValue, Func } from "./types/common"
 export { Error } from "./types/error"
 
-export { JSClient, NodeClient } from "./interfaces/client"
+export { JSClient } from "./interfaces/client"
 export { Breadcrumb } from "./interfaces/breadcrumb"
-export {
-  JSSpan,
-  NodeSpan,
-  JSSpanData,
-  NodeSpanOptions,
-  SpanContext
-} from "./interfaces/span"
-export { Tracer } from "./interfaces/tracer"
-export { Metrics } from "./interfaces/metrics"
-export { Plugin } from "./interfaces/plugin"
-export { Probes } from "./interfaces/probes"
+export { JSSpan, JSSpanData } from "./interfaces/span"
 export { Hook } from "./interfaces/hook"
