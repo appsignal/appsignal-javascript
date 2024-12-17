566445d72cc186d05e5ae0263b0022023fcf3890
Amiel Martin <amiel.martin@gmail.com>
2024-12-17 14:03:35 -0800

    Backfill tests for stimulus integration
    This creates a test file for the stimulus integration before adding a
    new feature. This way tests can be written to drive the new feature.

---
+++ packages/stimulus/src/__tests__/index.test.ts
@@ -0,0 +1,60 @@
+import { installErrorHandler } from "../index"
+import type { JSSpan } from "@appsignal/types"
+
+describe("Stimulus errorHandler", () => {
+  let appsignal: any
+
+  const mock: any = {
+    setAction: jest.fn(() => mock),
+    setError: jest.fn(() => mock),
+    setTags: jest.fn(() => mock)
+  }
+
+  const SpanMock = jest.fn().mockImplementation(() => mock)
+
+  beforeEach(() => {
+    appsignal = {
+      createSpan: (fn: (span: JSSpan) => void) => {
+        const mock = new SpanMock()
+        fn(mock)
+        return mock
+      },
+      send: jest.fn()
+    }
+  })
+
+  it("calls AppSignal helper methods", () => {
+    const err = new Error("test")
+    const message = "This is a test message"
+
+    const stimulusApplication: any = {}
+
+    installErrorHandler(appsignal, stimulusApplication)
+
+    stimulusApplication.handleError(err, message)
+
+    expect(mock.setTags).toBeCalledWith({
+      framework: "Stimulus",
+      message,
+    })
+
+    expect(mock.setError).toBeCalledWith(err)
+
+    expect(appsignal.send).toBeCalled()
+  })
+
+  it("calls any previously defined error handler", () => {
+    const err = new Error("test")
+    const message = "This is a test message"
+
+    const originalErrorHandler = jest.fn()
+    const stimulusApplication: any = { handleError: originalErrorHandler }
+
+    installErrorHandler(appsignal, stimulusApplication)
+
+    stimulusApplication.handleError(err, message)
+
+    expect(appsignal.send).toBeCalled()
+    expect(originalErrorHandler).toBeCalledWith(err, message)
+  })
+})
